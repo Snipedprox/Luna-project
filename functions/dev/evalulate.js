a25a55ed9b906dd50e3321890e7413f6f82401f9
Snipedprox <85262576+Snipedprox@users.noreply.github.com>
2021-10-03 16:30:29 -0300

    Add files via upload

---
+++ functions/dev/evalulate.js
@@ -0,0 +1,9 @@
+module.exports = {
+name: "eval",
+aliases: ['aoi' , 'ev'],
+code:`
+$onlyForids[$botownerid;870441674525012018;only my  owners may use this command]
+$eval[$message]
+$argsCheck[1>;provide me some text!]
+$deletecommand`}
+
